2ab4e30a889a69f6d255c056d27a50dd4c349ecf
Sebastian Montes <sebastianmontesolivera@gmail.com>
2026-06-03 16:29:23 -0500

    chore: eliminar paquetes duplicados en inglés (config, controller, service, exception, dto/events) y DTOs antiguos

--- src/main/java/com/restaurant/exception/OrderAlreadyPaidException.java
+++
@@ -1,7 +0,0 @@
-package com.restaurant.exception;
-
-public class OrderAlreadyPaidException extends RuntimeException {
-    public OrderAlreadyPaidException(String message) {
-        super(message);
-    }
-}
